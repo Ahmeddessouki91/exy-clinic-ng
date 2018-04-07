ad7b373428eb3d6eeaee86cc25964217f6840f6e
Ahmed dessouki <eng.ahmed.dessouki91@gmail.com>
2018-04-07 02:34:59 +0200

    add config and mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+require('./config/config');
 const app = require('./server');
 const http = require('http');
 
-const server = http.createServer(app);
-const port = process.env.PORT || 3000;
 
-server.listen(port, console.log("Server is listening on port 3000"));
+const server = http.createServer(app);
+const port = process.env.PORT;
+
+server.listen(port, console.log(`Server is listening on port ${port}`));
